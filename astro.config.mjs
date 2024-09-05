a604e89cbc0f7e78b129e348f4e3ef3e407021eb
Ismail Kalimi <ismailkalimi@gmail.com>
2024-09-05 16:45:47 -0500

    added dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
-// Import the Vercel adapter
-import vercel from '@astrojs/vercel/serverless';
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
-  site: 'https://isonmynetwork.github.io',
+  output: 'static',
+  site: 'https://isonmynet.work',
 });
