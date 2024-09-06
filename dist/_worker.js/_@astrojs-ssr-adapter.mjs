941df966ee11ac88406615808af2489333e16e72
Ismail Kalimi <ismailkalimi@gmail.com>
2024-09-05 20:27:58 -0500

    fixed menu bars

--- dist/_worker.js/_@astrojs-ssr-adapter.mjs
+++
@@ -1,2 +0,0 @@
-globalThis.process ??= {}; globalThis.process.env ??= {};
-export { c as createExports } from './chunks/_@astrojs-ssr-adapter_BFOR6U-0.mjs';
--- dist/_worker.js/middleware.mjs
+++
@@ -1,4 +0,0 @@
-globalThis.process ??= {}; globalThis.process.env ??= {};
-import './chunks/shared_DHCOKnMn.mjs';
-import './chunks/index_DMGOxStQ.mjs';
-export { onRequest } from './_astro-internal_middleware.mjs';
