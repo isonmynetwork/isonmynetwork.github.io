27891d524f8090a21321204e0d6459a46d6df96f
isOnMyNet.work <huzzyz@isonmynet.work>
2025-10-21 13:38:42 +0400

    Update astro.config.mjs
    fix trailing slash for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,5 @@ export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
   site: 'https://isonmynet.work',
+  trailingSlash: 'ignore', // 👈 This lets both /referral and /referral/ work
 });
